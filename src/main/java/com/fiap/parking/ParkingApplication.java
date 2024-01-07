9aca1156de76f32deecd4668de58c92977be6516
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-01-07 16:51:17 -0300

    Add suporte Resource Bundle

--- src/main/java/com/fiap/parking/ParkingApplication.java
+++ src/main/java/com/fiap/parking/ParkingApplication.java
@@ -2,7 +2,10 @@ package com.fiap.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,5 +15,11 @@ public class ParkingApplication {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
 
+	@Bean
+	LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
 
 }
---
+++ src/main/java/com/fiap/parking/infra/utils/Utils.java
@@ -0,0 +1,17 @@
+package com.fiap.parking.infra.utils;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+public class Utils {
+    final static ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+  
+    static {
+        source.setDefaultEncoding("UTF-8");
+        source.addBasenames("messages");
+    }
+
+    public static String getMessage(final String message) {
+        return source.getMessage(message, null, LocaleContextHolder.getLocale());
+    }
+}
